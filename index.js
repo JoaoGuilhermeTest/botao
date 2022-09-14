6acc1a90192fad5b5173252aebb7bcdd332731a3
JoaoGuilhermeTest <113489621+JoaoGuilhermeTest@users.noreply.github.com>
2022-09-14 01:20:07 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ somar.addEventListener("click", () => {
     const text = texto.innerText
 
     texto.innerText = text == "Série zerada" || text == "Começar Série" ? texto.innerText = '' :  texto.innerText
-    texto.innerText = text == '' || text == NaN ? texto.innerText = 1 + +texto.innerText : texto.innerText = 1 + +texto.innerText
+    texto.innerText = text == '' || texto > 0 ? texto.innerText = 1 + +texto.innerText : texto.innerText = 1 + +texto.innerText
     texto.innerText = text >= 5 ? texto.innerText = 0 :  texto.innerText
 
 })
