0bd4d8249b51df9f2887c54ecbeea27a615a4d00
JoaoGuilhermeTest <113489621+JoaoGuilhermeTest@users.noreply.github.com>
2022-09-14 00:54:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const somar = document.getElementById("sum")
 const texto = document.getElementById("texto")
 
 somar.addEventListener("click", () => {
-    if (texto.innerText == "Série zerada" || texto.innerText == "Começar Série") {
-        texto.innerText = ''
-        texto.innerText = 1 + +texto.innerText
-    } else {
-        texto.innerText = 1 + +texto.innerText
-    }
+    const text = texto.innerText
+
+    texto.innerText = text == "Série zerada" || text == "Começar Série" ? texto.innerText = '' :  texto.innerText
+    texto.innerText = text == '' || text == NaN ? texto.innerText = 1 + +texto.innerText : texto.innerText = 1 + +texto.innerText
+    texto.innerText = text >= 5 ? texto.innerText = 0 :  texto.innerText
+
 })
 zerar.addEventListener("click", () => {
     texto.innerText = "Série zerada"
+    setTimeout(() => {texto.innerText = "Começar Série"}, 700)
 })
